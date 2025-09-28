317683178c9e103f5469f5d0040d19f5df04295d
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-29 00:19:37 +0200

    Added lualine for prettier status line in nvim.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -10,4 +10,13 @@ return {
             enable_transparency()
         end
     },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = 'tokyonight',
+        }
+    }
 }
