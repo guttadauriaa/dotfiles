21855f1cb04c4d1eeb2a1951b39fbc79e0cc942c
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-29 00:17:04 +0200

    Added terminal's transparency support.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,8 +1,13 @@
+local function enable_transparency()
+    vim.api.nvim_set_hl(0,"Normal", { bg = "none" })
+end
+
 return {
     {
         "folke/tokyonight.nvim",
         config = function()
             vim.cmd.colorscheme "tokyonight"
+            enable_transparency()
         end
     },
 }
