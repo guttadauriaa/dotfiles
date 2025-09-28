36a75b20366c2eb9da0042312269211f19a27c70
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-29 00:52:15 +0200

    Added treesitter plugin.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "lua",
+                "c",
+                "cpp",
+                "java",
+                "html"
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotage = { enable = true },
+            auto_install = false,
+        })
+    end
+}
