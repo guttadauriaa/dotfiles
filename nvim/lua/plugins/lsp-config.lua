93bc9245505de366a35b7cb7f91aac7ca2218d2a
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-30 01:06:31 +0200

    Added ts_ls for TypeScript.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "bashls", "clangd" }
+                ensure_installed = { "lua_ls", "ts_ls", "bashls", "clangd" }
             })
         end
     },
@@ -17,11 +17,13 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             vim.lsp.config('lua_ls', {})
+            vim.lsp.config('ts_ls', {})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
             vim.lsp.enable({'lua_ls'})
+            vim.lsp.enable({'ts_ls'})
         end
     }
 }
