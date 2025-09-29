7984f032e160cdbd1aad50210680bc294bcbd633
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-29 23:47:39 +0200

    Added neo-tree plugin.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+        ---@module 'neo-tree'
+        ---@type neotree.Config
+        opts = {
+            vim.keymap.set('n', '<leader>e', '<Cmd>Neotree<CR>'),
+            popup_border_style = "",
+            enable_git_status = true,
+            enable_diagnostics = true,
+        }
+    }
+}
