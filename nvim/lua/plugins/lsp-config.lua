c012dc2e78f2a1897c08ae94f430939deca4aee5
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-30 01:05:24 +0200

    Moved keymaps out of lua_ls in nvim-lspconfig.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,11 +16,11 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.config('lua_ls', {
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}),
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}),
-                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {}),
-            })
+            vim.lsp.config('lua_ls', {})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
             vim.lsp.enable({'lua_ls'})
         end
     }
