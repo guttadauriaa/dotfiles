578352497c9730c9f4d3eb123f3e6452b873edce
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-09-29 23:40:16 +0200

    Added catppuccin colorscheme in neovim.
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -11,6 +11,12 @@ return {
         end
     },
     {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000
+    },
+    {
         "nvim-lualine/lualine.nvim",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
