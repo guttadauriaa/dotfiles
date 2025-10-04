5e2a7e2c4cada97ac3c324de47aaebeefda6cf05
Adriano Guttadauria <adriano.guttadauria@umons.ac.be>
2025-10-04 18:41:37 +0200

    Adding PageUp/PageDown support in tmux.
    And also vim/nvim when inside a tmux session.
    
    Signed-off-by: Adriano Guttadauria <adriano.guttadauria@umons.ac.be>

--- nvim/lua/config/globals.lua
+++ nvim/lua/config/globals.lua
@@ -4,3 +4,8 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have configured your terminal with a Nerd Font
 vim.g.have_nerd_font = true
+
+if $TERM =~ '\\v^(screen|tmux).*'
+  set <Home>=^[OH
+  set <End>=^[OF
+endif
